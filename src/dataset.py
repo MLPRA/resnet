f2725336b7b7c5b6f0f0e5df37e8c3ff5ba83749
Friedrich Schöne <schoene@livionda.com>
2018-01-09 14:25:20 +0100

    added trainer

--- src/dataset.py
+++ src/dataset.py
@@ -1,45 +1,17 @@
 import cv2
 import os
 import numpy as np
+import random
 import xml.etree.ElementTree as ElementTree
 
-class LabeledImageDataset():
 
-    def __init__(self, dir_paths):
-        jpg_paths = {}
-        xml_paths = {}
+class LabeledImageDataset:
 
-        for dir_path in dir_paths:
-            for root, _, files in os.walk(dir_path):
-                for file in files:
-                    file_name, file_extension = os.path.splitext(file)
-                    if file_extension.lower() == '.jpg':
-                        jpg_paths[file_name] = root + '/' + file
-                    elif file_extension.lower() == '.xml':
-                        xml_paths[file_name] = root + '/' + file
-
-        self.labeled_images = []
-
-        for key in xml_paths:
-            if key in jpg_paths:
-                xml = ElementTree.parse(xml_paths[key])
-                for object in xml.findall('object'):
-                    object_name = object.find('name').text
-                    object_label = abs(hash(object_name))
-
-                    object_xmin = int(object.find('bndbox/xmin').text)
-                    object_ymin = int(object.find('bndbox/ymin').text)
-                    object_xmax = int(object.find('bndbox/xmax').text)
-                    object_ymax = int(object.find('bndbox/ymax').text)
-
-                    labeled_image = LabeledImage(jpg_paths[key], object_label, object_xmin, object_ymin, object_xmax, object_ymax)
-                    self.labeled_images.append(labeled_image)
+    def __init__(self, labeled_images):
+        self.labeled_images = labeled_images
 
     def __len__(self):
         return len(self.labeled_images)
-
-    def get_example(self, index):
-        return self.labeled_images[index].img_label_pair()
 
     def __getitem__(self, index):
         if isinstance(index, slice):
@@ -51,7 +23,13 @@ class LabeledImageDataset():
         else:
             return self.get_example(index)
 
-class LabeledImage():
+    def get_example(self, index):
+        return self.labeled_images[index].img_label_pair()
+
+
+class LabeledImage:
+
+    IMG_SIZE = 300
 
     def __init__(self, path, label, xmin, ymin, xmax, ymax):
         self.path = path
@@ -70,19 +48,58 @@ class LabeledImage():
         image = image[self.ymin:self.ymax, self.xmin:self.xmax, :]
 
         height, width = image.shape[:2]
+        image = cv2.resize(image, (self.IMG_SIZE, self.IMG_SIZE))
 
-        # Resize image with its shorter side = 256
-        if height < width:
-            resize_height = 256
-            resize_width = width * resize_height // height
-        else:
-            resize_width = 256
-            resize_height = height * resize_width // width
-
-        image = cv2.resize(image, (resize_width, resize_height))
-
-        image = image.transpose(2, 0, 1).astype(np.int32)
+        image = image.transpose(2, 0, 1).astype(np.float32)
+        image *= (1.0 / 255.0)
 
         label = np.array(self.label, dtype=np.int32)
 
         return image, label
+
+
+class LabeledImageDatasetBuilder:
+
+    def __init__(self, dir_paths):
+        self.labeled_images = []
+
+        jpg_paths = {}
+        xml_paths = {}
+
+        for dir_path in dir_paths:
+            for root, _, files in os.walk(dir_path):
+                for file in files:
+                    file_name, file_extension = os.path.splitext(file)
+                    if file_extension.lower() == '.jpg':
+                        jpg_paths[file_name] = root + '/' + file
+                    elif file_extension.lower() == '.xml':
+                        xml_paths[file_name] = root + '/' + file
+
+        for key in xml_paths:
+            if key in jpg_paths:
+                xml = ElementTree.parse(xml_paths[key])
+                for object in xml.findall('object'):
+                    object_name = object.find('name').text
+                    object_label = abs(hash(object_name))
+
+                    object_xmin = int(object.find('bndbox/xmin').text)
+                    object_ymin = int(object.find('bndbox/ymin').text)
+                    object_xmax = int(object.find('bndbox/xmax').text)
+                    object_ymax = int(object.find('bndbox/ymax').text)
+
+                    labeled_image = LabeledImage(jpg_paths[key], object_label, object_xmin, object_ymin, object_xmax,
+                                                 object_ymax)
+                    self.labeled_images.append(labeled_image)
+
+        random.shuffle(self.labeled_images)
+
+    def get_labeled_image_dataset(self):
+        return LabeledImageDataset(self.labeled_images)
+
+    def get_labeled_image_dataset_split(self, splitsize):
+        splitnumber = int(round(len(self.labeled_images) * splitsize))
+
+        dataset1 = LabeledImageDataset(self.labeled_images[:splitnumber])
+        dataset2 = LabeledImageDataset(self.labeled_images[splitnumber:])
+
+        return dataset1, dataset2
--- src/run_train.py
+++ src/run_train.py
@@ -3,54 +3,58 @@ import chainer
 import multiprocessing
 from chainer.links import ResNet50Layers
 from progressbar import ProgressBar
-from src.dataset import LabeledImageDataset
+from src.dataset import LabeledImageDatasetBuilder
 
 def run_train():
 
     parser = ArgumentParser()
-    parser.add_argument('--batchsize', type=int, default=200)
-    parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--paths', type=str, nargs='+', required=True)
-
+    parser.add_argument('--paths', type=str, nargs='+', required=True,
+                        help='Root paths of folders that contain images and pascal voc files')
+    parser.add_argument('--training_splitsize', type=float, default=0.9,
+                        help='Splitsize of training data')
+    parser.add_argument('--batchsize', type=int, default=20,
+                        help='Learning minibatch size')
+    parser.add_argument('--epoch', type=int, default=10,
+                        help='Numbers of epochs to train')
+    parser.add_argument('--gpu', type=int, default=-1,
+                        help='GPU ID, negative value indicates CPU')
+    parser.add_argument('--out', default='output',
+                        help='Output directory')
+    parser.add_argument('--val_batchsize', type=int, default=250,
+                        help='Validation minibatch size')
     args = parser.parse_args()
 
-    device = 0 if args.gpu >= 0 else -1
-
+    # create model
     model = ResNet50Layers(None)
 
-    # load npz stuff here
-    # chainer.serializers.load_npz('{}.npz'.format('resnet50'), model)
+    # TODO: initmodel
 
-    model = chainer.links.Classifier(model)
-
-    # gpu
+    # use selected gpu by id
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
 
-    dataset = LabeledImageDataset(args.paths)
+    # TODO: mean
 
-    multiprocessing.set_start_method('spawn')
+    # build datasets from paths
+    builder = LabeledImageDatasetBuilder(args.paths)
+    train_dataset, val_dataset = builder.get_labeled_image_dataset_split(args.training_splitsize)
 
-    dataset_iter = chainer.iterators.MultiprocessIterator(dataset, args.batchsize, repeat=False)
+    train_iter = chainer.iterators.SerialIterator(train_dataset, args.batchsize, repeat=False)
+    val_iter = chainer.iterators.SerialIterator(val_dataset, args.val_batchsize, repeat=False)
 
-    # what does this mean ?
-    chainer.config.enable_backprop = False
-    chainer.config.train = False
+    # optimizer
+    learning_rate = 0.01
+    momentum = 0.9
+    optimizer = chainer.optimizers.MomentumSGD(learning_rate, momentum)
+    optimizer.setup(model)
 
-    sum_accuracy = 0
-    count = 0
+    # trainer
+    updater = chainer.training.updater.StandardUpdater(train_iter, optimizer, device=args.gpu)
+    trainer = chainer.training.Trainer(updater, (args.epoch, 'epoch'), args.out)
 
-    progress = ProgressBar(min_value=0, max_value=len(dataset)).start()
-    for batch in dataset_iter:
+    # TODO: extensions
 
-        x, t = chainer.dataset.concat_examples(batch, device=device)
-        x = chainer.Variable(x)
-        t = chainer.Variable(t)
-        loss = model(x, t)
-        sum_accuracy += float(model.accuracy.data)
-        count += len(t)
+    trainer.extend(chainer.training.extensions.ProgressBar(update_interval=10))
 
-        progress.update(count)
-
-    dataset_iter.finalize()
+    trainer.run()
